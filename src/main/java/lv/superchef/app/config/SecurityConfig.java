30fa55fe196590087cca0986c41e4231f9cfc58a
Adrian Moroz <adrianmoroz1999@gmail.com>
2026-07-13 15:23:38 +0300

    Connected frontend to backend.

--- src/main/java/lv/superchef/app/config/SecurityConfig.java
+++ src/main/java/lv/superchef/app/config/SecurityConfig.java
@@ -7,12 +7,8 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -24,21 +20,32 @@ public class SecurityConfig {
         http
                 .csrf(AbstractHttpConfigurer::disable)
                 .httpBasic(Customizer.withDefaults())
-                .formLogin(Customizer.withDefaults())
+                .formLogin(form -> form
+                        .loginPage("/login")
+                        .loginProcessingUrl("/login")
+                        .defaultSuccessUrl("/home", true)
+                        .failureUrl("/login?error")
+                        .usernameParameter("username")
+                        .passwordParameter("password")
+                        .permitAll())
+                .logout(logout -> logout
+                        .logoutSuccessUrl("/login?logout")
+                        .permitAll())
                 .authorizeHttpRequests(authorize -> authorize
-                        .requestMatchers("/register", "/css/**", "/js/**", "/images/**").permitAll()
-                        .requestMatchers("/h2-console/**").hasRole("ADMIN")
-                        .anyRequest().authenticated()
-                ).headers(headers -> headers
-                        .frameOptions(HeadersConfigurer.FrameOptionsConfig::sameOrigin)
-                );
+                        .requestMatchers("/", "/register", "/login", "/css/**", "/js/**", "/images/**")
+                        .permitAll()
+                        .requestMatchers("/h2-console/**")
+                        .hasRole("ADMIN")
+                        .anyRequest()
+                        .authenticated())
+                .headers(headers -> headers.frameOptions(HeadersConfigurer.FrameOptionsConfig::sameOrigin));
 
         return http.build();
     }
 
 
     @Bean
-    public PasswordEncoder passwordEncoder(){
+    public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
 }
