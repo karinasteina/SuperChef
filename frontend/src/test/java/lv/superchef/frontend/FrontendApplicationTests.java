a8bffefbd024142c17cb07bbb4767539d5a1e87d
Mikus Rakis <mikus.rakis@gmail.com>
2026-07-13 07:46:46 +0300

    Add frontend starter

---
+++ frontend/src/test/java/lv/superchef/frontend/FrontendApplicationTests.java
@@ -0,0 +1,13 @@
+package lv.superchef.frontend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FrontendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
