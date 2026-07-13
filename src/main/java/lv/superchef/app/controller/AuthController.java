b7c3693e16c2884cd1dcc76c11fd304620df779a
Adrian Moroz <adrianmoroz1999@gmail.com>
2026-07-13 10:45:04 +0300

    Naming changes.

--- src/main/java/lv/superchef/app/controller/AuthController.java
+++ src/main/java/lv/superchef/app/controller/AuthController.java
@@ -6,27 +6,24 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class AuthController {
-    @GetMapping("/login")
-    public String getControllerShowLogInPage(){
-        return "login-view";
-    }
-
     @GetMapping("/register")
-    public String getControllerRegisterUser(Model model){
-        model.addAttribute("registerForm", new RegisterRequest());
+    public String getControllerRegisterUser(Model model) {
+        model.addAttribute("registerRequest", new RegisterRequest());
         return "register-form-view";
     }
 
     @PostMapping("/register")
-    public String postControllerRegisterUser(@Valid RegisterRequest registerRequest, BindingResult result, Model model){
-       if(result.hasErrors()){
-           return "register-form-view";
-       }
-        return "redirect:home-view"; // smth like that
+    public String postControllerRegisterUser(@Valid @ModelAttribute("registerRequest") RegisterRequest registerRequest, BindingResult result) {
+        if (result.hasErrors()) {
+            return "register-form-view";
+        }
+
+        return "redirect:/home";
     }
 
 }
