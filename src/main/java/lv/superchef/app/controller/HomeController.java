70375abd0b0327508a16907cb80f2fc09662cf0c
Mikus Rakis <mikus.rakis@gmail.com>
2026-07-14 08:09:03 +0300

    Move fake data to separate config file

--- src/main/java/lv/superchef/app/controller/HomeController.java
+++ src/main/java/lv/superchef/app/controller/HomeController.java
@@ -1,12 +1,24 @@
 package lv.superchef.app.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import static lv.superchef.app.config.TempData.RECIPE_DATA;
 
 @Controller
 public class HomeController {
-    @GetMapping("/home")
-    public String getControllerShowHomePage(){
+
+    @GetMapping("/")
+    public String getControllerShowHomePage(Model model) {
+        model.addAttribute("activePage", "home");
         return "home-view";
+    }
+
+    @GetMapping("/feed")
+    public String feed(Model model) {
+        model.addAttribute("activePage", "feed");
+        model.addAttribute("recipes", RECIPE_DATA);
+        return "feed";
     }
 }
